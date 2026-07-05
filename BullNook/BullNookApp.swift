7d898bd8767113242b922e5bfe6a8570ca6e6686
yangzhuo <josh.yangzhuo@gmail.com>
2026-07-05 14:38:53 +0800

    Initial Commit

---
+++ BullNook/BullNookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullNookApp.swift
+//  BullNook
+//
+//  Created by Yangzhuo on 2026/7/5.
+//
+
+import SwiftUI
+
+@main
+struct BullNookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
